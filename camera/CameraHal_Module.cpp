397056acf2a6ba7c044d0f00e8259f2b3b1a2bd5
satish patel <tosatishpatel@gmail.com>
2020-02-19 20:56:28 +0530

    [CameraHal_Module]: start linkset up script based on camera selection
    Note:
    [TODO]: block back camera script as it will be on ducati
    
    Signed-off-by: satish patel <tosatishpatel@gmail.com>

--- camera/CameraHal_Module.cpp
+++ camera/CameraHal_Module.cpp
@@ -27,6 +27,7 @@
 #include "CameraProperties.h"
 #include "TICameraParameters.h"
 
+#include <cutils/properties.h>
 
 static android::CameraProperties gCameraProperties;
 static android::CameraHal* gCameraHals[MAX_CAMERAS_SUPPORTED];
@@ -590,6 +591,21 @@ int camera_device_open(const hw_module_t* module, const char* name,
             rv = -ENOMEM;
             goto fail;
         }
+        // [satish]: set prop based on camera id
+        // This is to setup the media link based on camera id
+        LOGI("camera_device open:cameraid:%d", cameraid);
+#if 0       
+ if(cameraid > 0) {
+            if(property_set("ctl.start", "fclink") < 0) {
+                LOGI("HACK: property set to camera 1 is failed");
+            }
+        } else {
+            if(property_set("ctl.start", "bclink") < 0) {
+                LOGI("HACK: property set to camera 0 is failed");
+            }
+        }
+        //
+#endif
 
         memset(camera_device, 0, sizeof(*camera_device));
         memset(camera_ops, 0, sizeof(*camera_ops));
